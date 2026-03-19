219e633ec75362a0c211113ae8d66c5817395aaf
Bismk11 <s22120167@alumnos.itsur.edu.mx>
2026-03-19 16:00:10 -0600

    Se agregó PHONE_STATE para que detecte llamadas mientras la app está cerrada

--- Unidad 2/MiTelefoniaComposable/app/src/main/java/net/ivanvega/mitelefoniacompose/MiReceiverTelefonia.kt
+++ Unidad 2/MiTelefoniaComposable/app/src/main/java/net/ivanvega/mitelefoniacompose/MiReceiverTelefonia.kt
@@ -3,63 +3,36 @@ package net.ivanvega.mitelefoniacompose
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.provider.Telephony
-import android.telephony.SmsMessage
+import android.telephony.SmsManager
+import android.telephony.TelephonyManager
 import android.util.Log
-import android.widget.Toast
 
+class MiReceiverTelefonia : BroadcastReceiver() {
 
-class MiReceiverTelefonia: BroadcastReceiver()
-{
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        
+        if (action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
+            val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+            
+            if (state == TelephonyManager.EXTRA_STATE_RINGING) {
+                val incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+                Log.d("AutoReply", "Llamada entrante detectada desde: $incomingNumber")
 
+                val sharedPreferences = context.getSharedPreferences("AutoReplyPrefs", Context.MODE_PRIVATE)
+                val savedNumber = sharedPreferences.getString("TARGET_NUMBER", "")
+                val savedMessage = sharedPreferences.getString("REPLY_MESSAGE", "")
 
-    override fun onReceive(p0: Context?, intent: Intent?) {
-        val action: String? = intent?.getAction()
-        //Uri uri = intent.getData();
-         var strMensaje: String = ""
-        //Uri uri = intent.getData();
-        if (action == Intent.ACTION_BOOT_COMPLETED) {
-        }
-        if (Intent.ACTION_INPUT_METHOD_CHANGED === action) {
-        }
-        if (action == Intent.ACTION_BOOT_COMPLETED) {
-        }
-        if (action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
-            val bndSMS: Bundle? = intent?.getExtras()
-            val pdus = bndSMS?.get("pdus") as Array<Any>?
-            val smms: Array<SmsMessage?> = arrayOfNulls<SmsMessage>(pdus!!.size)
-            for (i in smms.indices) {
-                smms[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
-                strMensaje +="${"Mensaje: " + smms[i]?.getOriginatingAddress()}\n" +
-                        "${smms[i]?.getMessageBody().toString()}"
-
-
+                if (!incomingNumber.isNullOrEmpty() && incomingNumber == savedNumber && !savedMessage.isNullOrEmpty()) {
+                    try {
+                        val smsManager: SmsManager = context.getSystemService(SmsManager::class.java)
+                        smsManager.sendTextMessage(incomingNumber, null, savedMessage, null, null)
+                        Log.d("AutoReply", "Respuesta automática enviada correctamente a $incomingNumber")
+                    } catch (e: Exception) {
+                        Log.e("AutoReply", "Error al enviar el SMS automático", e)
+                    }
+                }
             }
-            Log.d("MiBroadcast", strMensaje)
         }
-
-
-        //Log.d("MiBroadcast", action)
-        //Log.d("MiBroadcast", uri.toString());
-        //Log.d("MiBroadcast", uri.toString());
-        /*Toast.makeText(
-            context.getApplicationContext(),
-            action, Toast.LENGTH_SHORT
-        ).show()
-
-        Toast.makeText(
-            context.getApplicationContext(),
-            strMensaje, Toast.LENGTH_SHORT
-        ).show()*/
-
-        val sb = StringBuilder()
-        sb.append("Action: " + intent?.getAction() + "\n")
-        sb.append("URI: " + intent?.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n")
-        val log = sb.toString()
-        /*Log.d(TAG, log)
-        Toast.makeText(context, log, Toast.LENGTH_LONG).show()*/
-
-
     }
 }
