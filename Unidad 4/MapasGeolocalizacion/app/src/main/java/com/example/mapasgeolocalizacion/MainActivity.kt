6846c9db7b42384090ac5c4e481ad966141c6b87
Bismk11 <s22120167@alumnos.itsur.edu.mx>
2026-04-08 18:16:44 -0600

    Inicialización

---
+++ Unidad 4/MapasGeolocalizacion/app/src/main/java/com/example/mapasgeolocalizacion/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mapasgeolocalizacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mapasgeolocalizacion.ui.theme.MapasGeolocalizacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MapasGeolocalizacionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MapasGeolocalizacionTheme {
+        Greeting("Android")
+    }
+}
